119f858395919b0c6fe178fe92342c1e6088aa15
ShevamV <136719386+ShevamV@users.noreply.github.com>
2023-06-23 14:37:19 +0530

    shwell.py new version

--- shwell.py
+++ shwell.py
@@ -3,14 +3,26 @@ import os
 import pythowo
 
 while True:
-    text = input("(｡･ω･｡)ﾉ♡ > ")
-    if text.strip() == "cwear":
+    text = input("ShevamV-Code/Terminal--> ")
+    
+    if text.strip() == "clear":
         os.system('clear' if os.name == 'posix' else 'cls')
         continue
+        
     if text.strip() == "":
         continue
     result, error = pythowo.run("<stdin>", text)
-
+    
+    if text.strip() == "s.example":
+    	print("")
+    	print("Here is an example to coding Sython...")
+    	print("type('hello world')")
+    	print("hello world")
+    	print("")
+    	print("1000 + 922")
+    	print("1922")
+    	continue
+    
     if error:
         print(error.as_string())
     elif result:
