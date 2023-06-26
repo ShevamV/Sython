4bf40d5ebc6e906ea807ef4a29d15131a9d01d99
ShevamV <136719386+ShevamV@users.noreply.github.com>
2023-06-26 15:08:27 +0530

    Update shwell.py

--- code/shwell.py
+++ code/shwell.py
@@ -39,7 +39,7 @@ if error:
     print(error.as_string())
 
 elif result:
-    if len(result.elements) == 1
+    if len(result.elements) == 1:
        print(repr(result.elements[0]))
     else:
        print(repr(result))
