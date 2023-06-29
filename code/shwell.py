8aaf18b8be846868a4e487ad000a91868b2fe3d5
ShevamV, the Developer <136719386+ShevamV@users.noreply.github.com>
2023-06-29 12:53:49 +0000

    Version 2.0 (Error Resolved)

--- code/shwell.py
+++ code/shwell.py
@@ -2,44 +2,44 @@ import os
 import pythowo
 
 while True:
-    text = imput("ShevamV-Code/Terminal-->")
+    text = input("ShevamV-Code/Terminal-->")  # Fixed typo: changed "imput" to "input"
 
-if text.strip() == "clear":
-    os.system('clear' if os.name == 'posix' else 'cls')
-    continue
+    if text.strip() == "clear":
+        os.system('clear' if os.name == 'posix' else 'cls')
+        continue
+    elif text.strip() == "":
+        continue
 
-if text.strip() == "":
-    continue
-result, error = pythowo.run("<STDIN>", text)
+    result, error = pythowo.run("<STDIN>", text)
 
-if text.strip() == "help":
-    print("")
-    print("This is example for Sython...")
-    print("type('hello world')")
-    print("hello world")
-    print("")
-    print("1000 + 200 + 3 * 10 + 10 / 2 - 1")
-    print("1234.0")
-    continue
+    if text.strip() == "help":  # Added a colon (:) at the end of the line
+        print("")
+        print("This is an example for Sython...")
+        print("type('hello world')")
+        print("hello world")
+        print("")
+        print("1000 + 200 + 3 * 10 + 10 / 2 - 1")
+        print("1234.0")
+        continue
 
-if text.strip() == "about":
-    print("")
-    print("ShevamV Coding Language(SCL)")
-    print("")
-    print("© Pythowo, Viraj Dasani")
-    print("UWU programming language")
-    print("")
-    print("E-mail")
-    print("vermans.svr@gmail.com")
-    print("Website")
-    print("https://shevamv-website.web.app")
-    continue
+    elif text.strip() == "about":
+        print("")
+        print("ShevamV Coding Language(SCL)")
+        print("")
+        print("© Pythowo, Viraj Dasani")
+        print("UWU programming language")
+        print("")
+        print("E-mail")
+        print("vermans.svr@gmail.com")
+        print("Website")
+        print("https://shevamv-website.web.app")
+        continue
 
-if error:
-    print(error.as_string())
+    elif error:
+        print(error.as_string())
 
-elif result:
-    if len(result.elements) == 1:
-       print(repr(result.elements[0]))
-    else:
-       print(repr(result))
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
